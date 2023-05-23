8d6a04d7e12a4a514ca8cb833cee7de446e315e8
onlinekkabchi <onlinekkabchi@gmail.com>
2023-05-23 10:54:27 +0900

    [style] 폰트 변경 및 카카오 로그인 기능 비활성화

--- FE/front/src/components/LoginSimple.jsx
+++ FE/front/src/components/LoginSimple.jsx
@@ -1,27 +1,16 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { app, login, logout } from "../util/realm";
-
-const style = {
-  width: "180px",
-  height: "40px",
-  border: "1px solid black",
-  borderRadius: "5px",
-};
 
 function LoginSimple() {
   const [user, setUser] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogIn = async () => {
-    await login(email, password).then((result) => setUser(result));
-  };
+  const handleLogIn = async () => {};
 
-  const handleLogOut = async () => {
-    await logout().then((result) => setUser(result));
-  };
+  const handleLogOut = async () => {};
+
   return (
     <div className="login-simple">
       <p>Login Simply</p>
--- FE/front/src/components/NoteList.jsx
+++ FE/front/src/components/NoteList.jsx
@@ -24,20 +24,6 @@ export default function NoteList() {
     fetching();
   }, []);
 
-  // useEffect(() => {
-  //   async function fetching() {
-  //     const res = await fetch("/notes/api");
-  //     if (!res.ok) {
-  //       throw new Error("Network response was not ok");
-  //     }
-  //     const result = await res.json();
-  //     console.log("fetching");
-  //     console.log(result);
-  //   }
-
-  //   fetching();
-  // }, []);
-
   if (notes === null || notes === undefined)
     return <ul className={className.list}>노트 없음</ul>;
 
